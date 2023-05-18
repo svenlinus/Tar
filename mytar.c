d63027e81990aa91972e8d231ef761c765ace315
riyabadadare <rbadadar@calpoly.edu>
2023-05-18 13:29:25 -0700

    level-order DFS of argument

--- mytar.c
+++ mytar.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <libgen.h>
+#include <limits.h>
+
 typedef enum { false, true } bool;
 extern char *optarg;
+
+void directories_traversal(char *path);
+void add_to_tarfile(char *to_add);
+int isDirectory(char *path);
 
 void usage() {
   fprintf(stderr, "usage: mytar [ctxvS]f tarfile [ path [ ... ] ]\n");
@@ -12,6 +22,8 @@ void usage() {
 int main(int argc, char *argv[]) {
   char opt;
   int i = 0;
+  char *path;
+
   /* Option flags */
   char *output = NULL;
   bool create_archive = false, 
@@ -19,6 +31,11 @@ int main(int argc, char *argv[]) {
        extract_contents = false, 
        verbose = false, 
        strict = false;
+
+  if (argc <= 3) {
+    usage();
+  }
+
   /* Parse options and update flags */
   while ((opt = argv[1][i++])) {
     switch (opt) {
@@ -42,11 +59,86 @@ int main(int argc, char *argv[]) {
         break;
     }
   }
+
   printf("%s %d %d %d %d %d\n", output, create_archive, print_contents, extract_contents, verbose, strict);
   if (!output)
     usage();
   if (!create_archive && !print_contents && !extract_contents)
     usage();
 
+  /* passing the path through the traversal function */
+  path = argv[3];
+  printf("this is the path: %s\n", path);
+  directories_traversal(path);
   return 0;
+}
+
+/* level-order DFS of the directory */
+void directories_traversal(char *path) {
+
+  DIR *dir;
+  struct dirent *dir_read;
+  struct stat stat_buffer;
+  int i=0;
+  int num_traversals = 0;
+  char child_path[PATH_MAX][PATH_MAX];
+
+  if (isDirectory(path) != 0) {
+    /* opening the directory */
+    dir = opendir(path);
+    if (dir == NULL) {
+      perror("opendir");
+      exit(EXIT_FAILURE);
+    }
+  }
+  else {
+    /* base case for a file */
+    char file_path[PATH_MAX];
+    snprintf(file_path, PATH_MAX, "%s/%s", dirname(path), basename(path));
+    printf("path: %s\n", path);
+    add_to_tarfile(path);
+    return;
+  }
+
+  /* iterating through all the directory's subdirectories or files */
+  while((dir_read = readdir(dir)) != NULL) {
+    
+    /* ignoring self and parent */
+    if (strcmp(dir_read->d_name, ".") == 0 || strcmp(dir_read->d_name, "..") == 0) {
+      continue;
+    }
+
+    printf("curr name: %s\n", dir_read->d_name);
+    /* statting the current child */
+    snprintf(child_path[i], PATH_MAX, "%s/%s", path, dir_read->d_name);
+
+    if (stat(child_path[i], &stat_buffer) == -1) {
+      perror ("stat");
+      exit(EXIT_FAILURE);
+    }
+
+    printf("path: %s\n", child_path[i]);
+    add_to_tarfile(child_path[i]);
+    i++;
+    num_traversals++;
+  }
+
+  /* recursing for each child */
+  for (i=0; i<num_traversals; i++) {
+    directories_traversal(child_path[i]);
+  }
+}
+
+void add_to_tarfile(char *to_add) {
+  /* not yet implemented */
+}
+
+int isDirectory(char *path) {
+  struct stat sb;
+  if (stat(path, &sb) == -1) {
+    perror("stat");
+    exit(EXIT_FAILURE);
+  }
+  /* returns 0 if it's not a directory */
+  return S_ISDIR(sb.st_mode);
 }
