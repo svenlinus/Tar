bc3cd708444348c1f51f810a2f6894e40c5e8bc9
linustoernqvist <linus.tornqvist@triggerbee.com>
2023-05-20 20:52:58 -0700

    Read size, mtime, chksum, and typeflag

--- decode.c
+++ decode.c
@@ -2,18 +2,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "decode.h"
 #include "integer.h"
+#include "encode.h"
 
 void read_archive_header(char *header, struct header *info) {
   int header_index = 0;
-  int i = 0;
-
-  /** Testing purposes **/
-  int c;
-  while ((c = header[i++]) != EOF && i < 10000)
-    ;
-  printf("%d\n", i);
+  int i;
 
   /** Read name **/
   strncpy(info->name, header, 100);
@@ -38,7 +35,41 @@ void read_archive_header(char *header, struct header *info) {
   strncpy(id, header + header_index, 8);
   info->stat.st_gid = octal_to_int(id, 8);
   header_index += 8;
-  printf("%s %d %d %d\n", info->name, info->stat.st_mode, info->stat.st_uid, info->stat.st_gid);
+
+  /** Read size **/
+  char size[12];
+  strncpy(size, header + header_index, 12);
+  info->stat.st_size = octal_to_int(size, 12);
+  header_index += 12;
+
+  /** Read mtime **/
+  char mtime[12];
+  strncpy(mtime, header + header_index, 12);
+  info->stat.st_mtime = octal_to_int(mtime, 12);
+  header_index += 12;
+
+  /** Read chksum **/
+  char chksum[8];
+  strncpy(chksum, header + header_index, 8);
+  info->chksum = octal_to_int(chksum, 8);
+  /* Fill chksum field with spaces */
+  memset(header + header_index, ' ', 8);
+  header_index += 8;
+  /* Recalculate chksum */
+  int sum = 0;
+  for (i = 0; i < HEADER_LEN; i ++) {
+    sum += (unsigned char)header[i];
+  }
+  if (sum != info->chksum) {
+    fprintf(stderr, "Can't read header: corrupt header \nActual: %d chksum: %d\n", sum, info->chksum);
+    exit(EXIT_FAILURE);
+  }
+
+  /** Read typeflag **/
+  info->typeflag = header[header_index];
+  header_index += 1;
+
+  printf("%s %d %d %d %d %d %d %c\n", info->name, info->stat.st_mode, info->stat.st_uid, info->stat.st_gid, (int)info->stat.st_size, (int)info->stat.st_mtime, info->chksum, info->typeflag);
 }
 
 long int octal_to_int(char *input, size_t size) {
